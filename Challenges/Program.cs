4f616fc3e998973874d6a825f19d3ddad267748b
cnickels21 <cnickels_21@yahoo.com>
2020-05-03 12:36:07 -0500

    user array printed to console

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -7,10 +7,20 @@ namespace Challenges
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ArrayScoreFinder();
             Console.ReadLine();
         }
 
-
+        private static void ArrayScoreFinder()
+        {
+            int[] userArray = new int[5];
+            for (int i = 0; i < userArray.Length; i++)
+            {
+                Console.WriteLine("Enter a number between 1-10.");
+                userArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join(", ", userArray));
+        }
 
     }
 }
